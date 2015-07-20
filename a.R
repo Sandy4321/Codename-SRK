1a2f5d2b68bc65fb4493d3717cc964957053889d
Horopter <santoshdesai12@hotmail.com>
2015-07-20 17:52:08 +0530

    Extraction of tweets and wordcloud in R complete.

---
+++ a.R
@@ -0,0 +1,38 @@
+library("plyr")
+library("twitteR")
+library("ROAuth")
+library("bitops")
+library("NLP")
+library("RCurl")
+library("devtools")
+library("proto")
+library("gsubfn")
+library("tm")
+library("RColorBrewer")
+library("wordcloud")
+library("ggplot2")
+library("gridExtra")
+ck <- 'KjmhdjaaVwd8TJlj4FTDXk52E'
+cs <- 'IWhqFCUcPVxPcZB7YT3djd3NCvO7VAws8ulIqMl7GKDcWgPMLF'
+ak <- '136316586-9KvA0YQ48Ofi94GpncgLTpZLa0Cf3Xe1m1iS9eK3'
+as <- 'IkTUdZWIoHb7a1bX62MijCRfIKo4ySMX5IfJjePpyTEx5'
+setup_twitter_oauth(ck,cs,ak,as)
+search.string <- "#Bahubali -RT"
+no.of.tweets <- 1400
+tweets <- searchTwitter(search.string, n=no.of.tweets, lang="en")
+tweets.text <- sapply(tweets, function(x) x$getText())
+tweets.text <- gsub("rt", "", tweets.text)
+tweets.text <- gsub("@\\w+", "", tweets.text)
+tweets.text <- gsub("[[:punct:]]", "", tweets.text)
+tweets.text <- gsub("http\\w+", "", tweets.text)
+tweets.text <- gsub("[ |\t]{2,}", "", tweets.text)
+tweets.text <- gsub("^ ", "", tweets.text)
+tweets.text <- gsub(" $", "", tweets.text)
+tweets.text <- gsub("[^[:alnum:]///' ]", "", tweets.text)
+tweets.text.corpus <- Corpus(VectorSource(tweets.text))
+tweets.text.corpus <- tm_map(tweets.text.corpus, removeWords, stopwords(),lazy=TRUE)
+wordcloud(tweets.text.corpus,min.freq = 2, scale=c(7,0.5),colors=brewer.pal(8, "Dark2"),  random.color= TRUE, random.order = FALSE, max.words = 150)
+tweety <- do.call("rbind", lapply(tweets, as.data.frame))
+write.csv(tweety,file="tweets.csv")
+write.csv(tweets.text,file="tweets-text.txt")
+writeLines(as.character(tweets.text.corpus), con="twcorpus.txt")
